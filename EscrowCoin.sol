203e29eaf659127c6a9a21e0e69b5f65a8b5e8c8
Depauli1 <107186204+Depauli1@users.noreply.github.com>
2023-10-18 16:51:26 -0700

    Update EscrowCoin.sol

--- EscrowCoin.sol
+++ EscrowCoin.sol
@@ -1,1 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract EscrowCoin is ERC20 {
+    constructor(uint256 initialSupply) ERC20("EscrowCoin", "EsC") {
+        _mint(msg.sender, initialSupply);
+    }
+}
